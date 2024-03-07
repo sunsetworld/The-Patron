2a5b37a63488a949729d7d9666a94fed211177cc
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2024-03-07 21:55:13 +0000

    Added toast

---
+++ Assets/Scripts/Toast/Toast.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+public class Toast : MonoBehaviour
+{
+    private Rigidbody2D _toastRb;
+    [SerializeField] private float toastMovementSpeed = 5;
+
+    private void Start()
+    {
+        _toastRb = GetComponent<Rigidbody2D>();
+        _toastRb.velocity = transform.up * toastMovementSpeed * Time.deltaTime;
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        DestroyBread(other.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DestroyBread(other.gameObject);
+    }
+
+    private void DestroyBread(GameObject other)
+    {
+        if (other.CompareTag("Player")) return;
+        if (other.CompareTag("Toaster")) return;
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/Toaster/toaster.cs
+++ Assets/Scripts/Toaster/toaster.cs
@@ -1,18 +1,62 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class toaster : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject bread;
+    [SerializeField] private float cookTime = 3;
+    [SerializeField] private Transform breadPoint;
+    [SerializeField] private Sprite toastUp;
+    [SerializeField] private Sprite toastDown;
+    private SpriteRenderer _spriteRenderer;
+    private bool _canShootBread = true;
+    private bool _playerIsUsingToaster;
+
+    private void Start()
     {
-        
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnInteract(InputAction.CallbackContext context)
     {
-        
+        if (!context.performed) return;
+        if (!_playerIsUsingToaster) return;
+        if (!_canShootBread) return;
+        StartCoroutine(ShootOutBread());
+    }
+
+    IEnumerator ShootOutBread()
+    {
+        _canShootBread = false;
+        _spriteRenderer.sprite = toastDown;
+        yield return new WaitForSeconds(cookTime);
+        if (bread == null)
+        {
+            Debug.LogWarning("There is no bread >:(");
+            Debug.Break();
+        }
+        else
+        {
+            _spriteRenderer.sprite = toastUp;
+            Instantiate(bread, breadPoint.position, transform.rotation);
+            StartCoroutine(ResetToaster());
+        }
+    }
+
+    IEnumerator ResetToaster()
+    {
+        yield return new WaitForSeconds(cookTime);
+        _canShootBread = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player")) _playerIsUsingToaster = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player")) _playerIsUsingToaster = false;
     }
 }
